c824e1f6abc349e44e81731286c1b2e65d4d4bd1
OleksiiMusiienko <musienko1984@gmail.com>
2023-12-26 11:12:19 +0200

    #Task28 Добавление класса Wrapper
    Класс оборачивает User и команду для передачи на сервер

---
+++ Wrapper.cs
@@ -0,0 +1,27 @@
+﻿using MessengerModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDLL
+{
+    [DataContract]
+    public class Wrapper
+    {
+        public enum Commands {Registratioin, Authorization, Redact}
+        [DataMember]
+        public User user { get; set; }
+        [DataMember]
+        public Commands commands { get; set; }
+        
+        public Wrapper() { }
+        public Wrapper(User user, Commands commands)
+        {
+            this.user = user;
+            this.commands = commands;
+        }
+    }
+}
