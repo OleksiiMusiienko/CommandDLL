6f5b00bb651d14cc04299b1fa7367b548391bce0
Виктория <babenko.viktoria.v@gmail.com>
2023-12-28 13:38:24 +0200

    #Task 3 Добавление класса ServerResponse
    Класс оборачивает List<User> и ответ сервера клиенту

---
+++ ServerResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDLL
+{
+    [DataContract]
+    public class ServerResponse
+    {
+        [DataMember]
+        public List<User> list=new List<User>();
+        [DataMember]
+        public string command { get; set; }
+
+        public ServerResponse() { }
+        public ServerResponse(List<User> us, Commands com)
+        {
+            list = us;
+            command = com;
+        }
+    }
+}
