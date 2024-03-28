a486ed3927129d409b8ddfbaf602fdf79ecb3463
Виктория <babenko.viktoria.v@gmail.com>
2024-03-28 20:23:43 +0200

    #Task 15 Создание базавого класса MesWrapper  и производные классы ListMessage и mMessage .
    Для отправки ответа сервера клиенту в соединении для передачи сообщений

---
+++ MesWrapper.cs
@@ -0,0 +1,18 @@
+﻿using MessengerModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDLL
+{
+    [DataContract]
+    public class MesWrapper
+    {
+        [DataMember]
+        public int len { get; set; }
+        public MesWrapper() { }
+    }
+}
---
+++ mMessage.cs
@@ -0,0 +1,18 @@
+﻿using MessengerModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDLL
+{
+    [DataContract]
+    public class mMessage : MesWrapper
+    {
+        [DataMember]
+        public Message Mes {  get; set; }
+        public mMessage() : base() { }
+    }
+}
